f1e32915959d26c7c90691216b64a00b25d3ca9f
Patrick Simard <patrick.simard@webrussell.com>
2020-12-02 12:46:26 -0500

    Adding footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,8 @@ class HomeController < ApplicationController
     @formations = Formation.order('id ASC')
     @langues = Langue.order('id ASC')
     @projets = Projet.order('id ASC')
-    @articles = Article.order('id ASC')
+    @articles = Article.where('id != ?','3').order('id ASC')
+    @rails = Article.where('id = ?','3').first
 
   end
 end
